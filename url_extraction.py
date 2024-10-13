f5645ab1c4af8ed97b337866e8602c30d7e0bf2f
AnoraWu <wanru@uchicago.edu>
2024-10-13 14:27:30 -0500

    Update url_extraction.py

--- url_extraction.py
+++ url_extraction.py
@@ -33,36 +33,40 @@ def extract_words(url):
     return text
 
 if __name__ == '__main__':
-    filename =  '微博数据_人影作业_10-13'
-    df = pd.read_csv(filename+'.csv')
-    df["网页信息"] = ""
 
-    # Iterate over each row, except the first one
-    for index, row in df.iloc[1:].iterrows():
-        web_content = []
-        urls = row["网页链接"]
-        
-        # Safely parse the list of URLs
-        try:
-            url_list = ast.literal_eval(urls)
-        except (SyntaxError, ValueError) as e:
-            print(f"Error parsing URLs in row {index}: {e}")
-            url_list = []
+    filename_list = ['微博数据_人影办_10-13','微博数据_人影办_14-17','微博数据_人影办_18-20','微博数据_人影办_21-23']
+    
+    for filename in filename_list:
 
-        # Extract content from each URL in the list
-        if url_list != []:
-            for url in url_list:
-                try:
-                    content = extract_words(url)
-                    web_content.append(content)
-                except Exception as e:
-                    print(f"Error fetching content from {url}: {e}")
-                    web_content.append('Error')
-        else:
-            continue
+        df = pd.read_csv(filename+'.csv',lineterminator='\n')
+        df["网页信息"] = ""
 
-        # Update DataFrame with the content for the current row
-        df.at[index, "网页信息"] = web_content
-        
-        # Save the updated DataFrame row by row (with append mode)
-        df.iloc[[index]].to_csv("{name}_带网页信息.csv".format(name=filename), mode='a', header=not index, index=False)
+        # Iterate over each row, except the first one
+        for index, row in df.iloc[1:].iterrows():
+            web_content = []
+            urls = row["网页链接"]
+            
+            # Safely parse the list of URLs
+            try:
+                url_list = ast.literal_eval(urls)
+            except (SyntaxError, ValueError) as e:
+                print(f"Error parsing URLs in row {index}: {e}")
+                url_list = []
+
+            # Extract content from each URL in the list
+            if url_list != []:
+                for url in url_list:
+                    try:
+                        content = extract_words(url)
+                        web_content.append(content)
+                    except Exception as e:
+                        print(f"Error fetching content from {url}: {e}")
+                        web_content.append('Error')
+            else:
+                continue
+
+            # Update DataFrame with the content for the current row
+            df.at[index, "网页信息"] = web_content
+            
+            # Save the updated DataFrame row by row (with append mode)
+            df.iloc[[index]].to_csv("{name}_带网页信息.csv".format(name=filename), mode='a', header=not index, index=False)
