467b948e524541805eb4a90c7fd9267e61c5a14f
AnoraWu <wanru@uchicago.edu>
2024-10-18 14:27:49 -0500

    判断气象局数据是否相关 - 切片

--- code/气象局数据multiple threads/AI_extraction_step1 2.py
+++ code/气象局数据multiple threads/AI_extraction_step1 2.py
@@ -19,6 +19,7 @@ def get_qwen_response(row):
     4. 一个返回'True'的例子是：'3月16日，湖北荆门沙尘来袭，出现中度以上污染。荆门市气象局迅速开展“人工增雨”作业，沉降空气中的扬沙与浮尘，发射增雨“火箭弹”四枚，人工增雨后效果明显。据荆门市气象局副局长苏磊介绍，这次适时开展人工增雨作业，对改善空气质量起到一定帮助'
     5. 一个返回'False'的例子是：'科学调度服务生态发展，增雨江淮共筑美好安徽”。为感谢民航安徽空管局对我省人工影响天气工作的支持，4月10日下午，省局党组成员、副局长包正擎带领减灾处、人影办和信息公司负责人赴民航安徽空管局对接工作，并赠送锦旗。民航安徽空管局副局长丁浩及相关处室和单位负责人参加了座谈会。双方围绕重大活动、抗旱和蓝天保卫战等人工影响天气活动空域管制协调，以及航空气象服务合作等方面进行了深入交流。'
     6. 如果难以确定，返回’True‘
+    7. 如果包含其他格式，例如，xml文档，返回'True'
     """
     # Send the request to the model
     # client = OpenAI(
@@ -40,43 +41,41 @@ if __name__ == "__main__":
     
     logging.basicConfig(filename="log.txt", level=logging.ERROR)  # 设置日志文件
 
-    # 读取原始 CSV 文件
-    for i in range(0,111):
-        os.chdir("/Users/anorawu/Documents/GitHub/CloudSeeding/data/气象局数据")
-        df = pd.read_csv("气象局数据_{num}.csv".format(num=i), encoding="utf-8")
+    os.chdir("/Users/anorawu/Documents/GitHub/CloudSeeding/data/气象局数据")
+    df = pd.read_csv("data_bureau_2.csv", encoding="utf-8")
+    
+    start_index = 1471  # 设置开始分析的索引
+
+    # 如果存在之前保存的索引，可以从中加载
+    try:
+        with open("index.txt", "r") as f:
+            start_index = int(f.read().strip())
+    except FileNotFoundError:
+        pass
+
+    # 遍历每一行并处理
+    os.chdir("/Users/anorawu/Documents/GitHub/CloudSeeding/data/气象局数据output")
+    with open("output_file_2.csv", "a", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        # 写入列名
+        if start_index == 0:
+            writer.writerow(list(df.columns))
         
-        start_index = 0  # 设置开始分析的索引
+        for index, row in df.iloc[start_index:].iterrows():  # 从指定的索引开始
 
-        # 如果存在之前保存的索引，可以从中加载
-        try:
-            with open("index.txt", "r") as f:
-                start_index = int(f.read().strip())
-        except FileNotFoundError:
-            pass
-
-        # 遍历每一行并处理
-        os.chdir("/Users/anorawu/Documents/GitHub/CloudSeeding/data/气象局数据output")
-        with open("output_file_{num}.csv".format(num=i), "a", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            # 写入列名
-            if start_index == 0:
-                writer.writerow(list(df.columns))
-            
-            for index, row in df.iloc[start_index:].iterrows():  # 从指定的索引开始
-
-                try:
-                    result = get_qwen_response(row).split()[0]
-                    print(index)
-                    print(result)
-                    if result == 'True':
-                        new_row = row.copy()
-                        writer.writerow(new_row.tolist())
+            try:
+                result = get_qwen_response(row)
+                print(2)
+                print(index)
+                print(result)
+                if 'True' in result:
+                    writer.writerow(row.tolist())
 
 
-                except Exception as e:
-                    logging.error(f"Error processing row {index}: {e}")
-                    print(f"Error processing row {index}: {e}")
-                    # 保存当前索引
-                    with open("index.txt", "w") as f:
-                        f.write(str(index))
-                    continue  # 继续处理下一个数据行
+            except Exception as e:
+                logging.error(f"Error processing row {index}: {e}")
+                print(f"Error processing row {index}: {e}")
+                # 保存当前索引
+                with open("index.txt", "w") as f:
+                    f.write(str(index))
+                continue  # 继续处理下一个数据行
---
+++ code/气象局数据multiple threads/Multithread 6-11.py
@@ -0,0 +1,39 @@
+from threading import Thread
+import subprocess
+import os
+
+# Set up directory
+os.chdir('/Users/anorawu/Documents/GitHub/CloudSeeding/code/微博数据multiple threads')
+
+t1 = Thread(target=subprocess.run, args=(["python", "AI_extraction_step1 6.py"],))
+t2 = Thread(target=subprocess.run, args=(["python", "AI_extraction_step1 7.py"],))
+t3 = Thread(target=subprocess.run, args=(["python", "AI_extraction_step1 8.py"],))
+t4 = Thread(target=subprocess.run, args=(["python", "AI_extraction_step1 9.py"],))
+t5 = Thread(target=subprocess.run, args=(["python", "AI_extraction_step1 10.py"],))
+t6 = Thread(target=subprocess.run, args=(["python", "AI_extraction_step1 11.py"],))
+# t5 = Thread(target=subprocess.run, args=(["python", "人工影响天气/2010-2013.py"],))
+# t6 = Thread(target=subprocess.run, args=(["python", "人工影响天气/2014-2017.py"],))
+# t7 = Thread(target=subprocess.run, args=(["python", "人工影响天气/2018-2020.py"],))
+# t8 = Thread(target=subprocess.run, args=(["python", "人工影响天气/2021-2023.py"],))
+
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+t6.start()
+# t7.start()
+# t8.start()
+
+
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+t5.join()
+t6.join()
+# t7.join()
+# t8.join()
+
+
