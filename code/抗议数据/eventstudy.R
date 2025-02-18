e02997943b606dbba7d1dd83ac453a58f3307ac8
AnoraWu <wanru@uchicago.edu>
2025-02-17 18:35:00 -0600

    update Feb 17th

--- code/抗议数据/eventstudy.R
+++ code/抗议数据/eventstudy.R
@@ -1,10 +1,18 @@
 library(fixest)   
+library(tidyverse) 
 library(dplyr)
 library(did)
 
 setwd("~/BFI Dropbox/Wanru Wu/Cloudseeding/data/抗议数据")
 data <- read.csv("eventstudy_data.csv")
+data <- data %>% 
+  mutate(event_date = if_else(is.na(event_date), 10000,
+                              event_date)) %>% 
+  mutate(to_day = if_else(is.na(to_day), 10000,
+                              to_day)) %>% 
+  select(n_cloudseeding, rainfall, event_date,to_day,id,date, citycode)
 
-data$event_date <- as.factor(data$event_date)
-
-feols(n_cloudseeding ~ rainfall+sunab(event_date, to_day) | id + date, data=data,cluster = ~id)
+fixest::setFixest_nthreads(8)
+res_sunab=fixest::feols(n_cloudseeding ~ rainfall+sunab(event_date, to_day, 
+                                                        ref.c = 10000, ref.p = c(-1, -2))
+                        | citycode + date, data=data,cluster = ~citycode)
