d9359da3b748a38143371af2886b55a2744c261d
AnoraWu <wanru@uchicago.edu>
2026-01-12 13:59:47 -0600

    Update merge_town_to_grid.py

--- code/energy_flux/construct_5km_data/merge_town_to_grid.py
+++ code/energy_flux/construct_5km_data/merge_town_to_grid.py
@@ -18,35 +18,39 @@ os.chdir('/Users/anora/Team MG Dropbox/Wanru Wu/Cloudseeding_Anora')
 
 ### CONSTRUCT GRID ###
 
-# Load and project JX polygon to EPSG:4527 
+# Load and project JX polygon to EPSG:32650 
 jx_poly = gpd.read_file('jiangxi/jiangxi_shape.shp').geometry.iloc[0]
-jx_poly_proj = gpd.GeoSeries([jx_poly], crs="EPSG:4326").to_crs("EPSG:4527").iloc[0]
+# Original jx_poly was in "EPSG:4326", convert it to "EPSG:32650" to construct grids in kilometers
+# "EPSG:32650" is used between between 114°E and 120°E
+jx_poly_proj = gpd.GeoSeries([jx_poly], crs="EPSG:4326").to_crs("EPSG:32650").iloc[0]
 
 # Calculate the bound of JX province            
 minx, miny, maxx, maxy = jx_poly_proj.bounds              
 
-# Bins for grid
+# Bins for 5km grid
 grid_size = 5000
+# Create boundaries for grids
+# The last bin created by np.arange will cover the maxx and maxy
 x_bins = np.arange(minx, maxx + grid_size, grid_size)
 y_bins = np.arange(miny, maxy + grid_size, grid_size)
 
+# Create box geometries that covered all jx_poly_proj based on generated bins 
 polygons = []
 for i in range(len(y_bins)-1):
     for j in range(len(x_bins)-1):
         # create 5km * 5km grids
         poly = box(x_bins[j], y_bins[i], x_bins[j+1], y_bins[i+1])
         polygons.append({
-            "cell_id": f"{i}_{j}",
+            "cell_id": f"{i}_{j}", # use i_j here because y_bins are row numbers while x_bins are column numbers
             "geometry": poly,
             "cell_y": i,
             "cell_x": j
         })
+jx_grid = gpd.GeoDataFrame(polygons, crs="EPSG:32650")
 
-jx_grid = gpd.GeoDataFrame(polygons, crs="EPSG:4527")
-
-# Filtering out grids outside JX
+# Filtering out grids outside JX, only keeping grids whose area of intersection with JX is larger than 0
 with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=RuntimeWarning, message="invalid value encountered in intersects")
+    warnings.filterwarnings("ignore", category=RuntimeWarning, message= "invalid value encountered in intersects")
     jx_grid = jx_grid[jx_grid.geometry.intersection(jx_poly_proj).area > 0]
 
 # Save as GeoPackage 
@@ -71,16 +75,20 @@ operation_data['date'] = pd.to_datetime(operation_data['date'])
 operation_data['day'] = operation_data['date'].dt.dayofyear
 operation_data['year'] = operation_data['date'].dt.year
 
-# Transfer lat and lon to "EPSG:4527"
-transformer = Transformer.from_crs("EPSG:4326", "EPSG:4527", always_xy=True)
+# Transfer lat and lon to EPSG:32650
+# "always_xy=True" ensures using the traditional GIS order, 
+# that is longitude, latitude for EPSG:4326 and easting, northing for EPSG:32650
+transformer = Transformer.from_crs("EPSG:4326", "EPSG:32650", always_xy=True)
 operation_data['xs'], operation_data['ys'] = transformer.transform(
     operation_data['lon'].values, 
     operation_data['lat'].values
 )
 
+# Get cell_x and cell_y, which starts from 0 (rather than 1)
 operation_data['cell_x'] = (np.array(operation_data['xs'])-minx)//grid_size 
 operation_data['cell_y'] = (np.array(operation_data['ys'])-miny)//grid_size 
 
+# Generate cell_id of the cell each operation data is in
 operation_data['cell_id'] = (
     operation_data['cell_y'].astype(int).astype(str) + "_" + 
     operation_data['cell_x'].astype(int).astype(str)
